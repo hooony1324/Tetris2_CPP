0f9a646de7b0acdda9a18bcc4512e70bfe69b33a
hooony1324 <78678316+hooony1324@users.noreply.github.com>
2021-11-25 19:36:01 +0900

    initial commit

---
+++ Tetris2_CPP/TetrisGame/TetrisGame.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "ConsoleHelper.h"
+
+int main()
+{
+    uint64 lastTick = 0;
+    while (true)
+    {
+#pragma region 프레임 관리
+        const uint64 currentTick = ::GetTickCount64();
+        const uint64 deltaTick = currentTick - lastTick;
+        lastTick = currentTick;
+#pragma endregion
+
+        // 입력
+
+        // 로직
+
+        // 렌더링
+
+        ConsoleHelper::SetCursorPosition(0, 0);
+        ConsoleHelper::ShowConsoleCursor(false);
+        ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+
+        const char* TILE = "□";
+        const char* TILE_BLACK = "■";
+
+        for (int32 y = 0; y < 20; y++)
+        {
+            for (int32 x = 0; x < 20; x++)
+            {
+                if (x > 5 && x < 15 && y > 5 && y < 15)
+                {
+                    ConsoleHelper::SetCursorColor(ConsoleColor::BLUE);
+                    cout << TILE_BLACK;
+                    ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+                    continue;
+                }
+                cout << TILE;
+            }
+
+            cout << endl;
+        }
+    }
+
+}
---
+++ Tetris2_CPP/TetrisGame/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include "Types.h"
+
+using namespace std;
+
+
+
